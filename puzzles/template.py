674ca2450cf3ad784217d124c289da1a7afe3b34
James Hoffman <jamesvhoffman@outlook.com>
2022-12-02 19:58:28 -0500

    Added template

---
+++ puzzles/template.py
@@ -0,0 +1,30 @@
+# Setting up the data --------------------------------------------------------
+
+# Read the input file
+# {reading the input goes here}
+
+# PART ONE -------------------------------------------------------------------
+
+# Method 1 - {task goes here}
+p1_solution = None
+
+# Alternate Method 2
+p1_alt1 = None
+
+# Output for PART ONE
+print("---PART ONE---")
+print("Answer to puzzle method 1:", p1_solution)
+print("Alternate method 2 answer:", p1_alt1)
+
+# PART TWO -------------------------------------------------------------------
+
+# Method 1 - {task goes here}
+p2_solution = None
+
+# Alternate Method 2
+p2_alt1 = None
+
+# Output for PART TWO
+print("---PART TWO---")
+print("Answer to puzzle method 1:", p2_solution)
+print("Alternate method 2 answer:", p2_alt1)
