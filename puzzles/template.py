a1951b5eee4501d15ef5ddcb905a7d015573260e
James Hoffman <jamesvhoffman@outlook.com>
2022-12-04 02:44:29 -0500

    Update to template

--- puzzles/template.py
+++ puzzles/template.py
@@ -1,30 +1,27 @@
+import os
+
+# Testing data ---------------------------------------------------------------
+test = None
+
 # Setting up the data --------------------------------------------------------
 
 # Read the input file
-# {reading the input goes here}
+path = os.getcwd() + r"\puzzles\INSERT_DAY_HERE\input"
+with open(path) as f:
+    lines = f.read()
 
 # PART ONE -------------------------------------------------------------------
 
-# Method 1 - {task goes here}
 p1_solution = None
-
-# Alternate Method 2
-p1_alt1 = None
 
 # Output for PART ONE
 print("---PART ONE---")
-print("Answer to puzzle method 1:", p1_solution)
-print("Alternate method 2 answer:", p1_alt1)
+print("Answer to puzzle:", p1_solution)
 
 # PART TWO -------------------------------------------------------------------
 
-# Method 1 - {task goes here}
 p2_solution = None
-
-# Alternate Method 2
-p2_alt1 = None
 
 # Output for PART TWO
 print("---PART TWO---")
-print("Answer to puzzle method 1:", p2_solution)
-print("Alternate method 2 answer:", p2_alt1)
+print("Answer to puzzle:", p2_solution)
